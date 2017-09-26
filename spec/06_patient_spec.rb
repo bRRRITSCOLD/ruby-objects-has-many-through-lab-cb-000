de6a66b897941327c589cd5dbd186f96f1fab6c1
Blaine Richardson <blainerrichardson@gmail.com>
2017-09-26 16:19:55 +0000

    automatically backed up by learn

--- spec/06_patient_spec.rb
+++ spec/06_patient_spec.rb
@@ -1,23 +1,28 @@
-class Patient
-  attr_accessor :name
+require "spec_helper"
 
-  def initialize(name)
-    @name = name
-    @appointments = []
+describe "Patient" do
+  let!(:martin) { Patient.new("Martin Jones") }
+  let!(:doctor_who) { Doctor.new("The Doctor") }
+  let!(:appointment) { Appointment.new("Friday, January 32nd", doctor_who) }
+
+  describe "#new" do
+    it "initializes with a name" do
+      expect{Patient.new("Sophie")}.to_not raise_error
+    end
   end
 
-  def add_appointment(appointment)
-    @appointments << appointment
-    appointment.patient = self
+  describe "#add_appointment" do
+    it "takes in an argument of an apppointment and adds that appointment to it's list of appointments and tells that appointment it belongs to the patient" do
+      martin.add_appointment(appointment)
+      expect(martin.appointments).to include(appointment)
+      expect(appointment.patient).to eq(martin)
+    end
   end
 
-  def appointments
-    @appointments
-  end
-
-  def doctors
-    self.appointments.collect do |appointment|
-      appointment.doctor
+  describe "#doctors" do
+    it "has many doctors through appointments" do
+      martin.add_appointment(appointment)
+      expect(martin.doctors).to include(doctor_who)
     end
   end
 end
