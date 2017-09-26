a3b0edbe73b6b7af5b1b4e90bf96549297ef5e34
Blaine Richardson <blainerrichardson@gmail.com>
2017-09-26 16:17:51 +0000

    automatically backed up by learn

--- spec/04_doctor_spec.rb
+++ spec/04_doctor_spec.rb
@@ -1,30 +1,52 @@
 require "spec_helper"
 
-describe "Appointment" do
-  
-  describe "#new" do 
-    it "initializes with a date and a doctor" do 
-      doctor_who = Doctor.new("The Doctor")
-      expect{Appointment.new("Thursday, October 31st", doctor_who)}.to_not raise_error
+describe "Doctor" do
+
+  describe "#new" do
+    it "initializes with a name and an empty collection of appointments" do
+      doctor_smith = Doctor.new("John Smith")
+      expect{ Doctor.new("Martha Jones")}.to_not raise_error
+      expect(doctor_smith.instance_variable_get(:@appointments)).to eq([])
     end
   end
 
-  describe "#patient" do
-    it "belongs to a patient" do 
+  describe "#name" do
+    it "has a name" do
+      doctor_who = Doctor.new("The Doctor")
+      expect(doctor_who.name).to eq("The Doctor")
+    end
+  end
+
+  describe "#add_appointment" do
+    it "adds a new appointment to the doctor's @appointments  array and tells that appointment that it belongs to the doctor" do
+      doctor_who = Doctor.new("The Doctor")
+      appointment = Appointment.new("Friday, January 32nd", doctor_who)
+      doctor_who.add_appointment(appointment)
+
+      expect(doctor_who.instance_variable_get(:@appointments)).to include(appointment)
+      expect(appointment.doctor).to eq(doctor_who)
+    end
+  end
+
+  describe "#appointments" do
+    it "has many appointment" do
+      doctor_who = Doctor.new("The Doctor")
+      appointment = Appointment.new("Friday, January 32nd", doctor_who)
+      doctor_who.add_appointment(appointment)
+
+      expect(doctor_who.appointments).to include(appointment)
+    end
+  end
+
+  describe "#patients" do
+    it "has many patients, through appointments" do
       doctor_who = Doctor.new("The Doctor")
       appointment = Appointment.new("Friday, January 32nd", doctor_who)
       martin = Patient.new("Martin Jones")
-      appointment.patient = martin
-      expect(appointment.patient).to eq(martin)
-    end 
-  end
+      martin.add_appointment(appointment)
 
-  describe "#doctor" do 
-    it "belongs to a doctor" do 
-      doctor_who = Doctor.new("The Doctor")
-      appointment = Appointment.new("Friday, January 32nd", doctor_who)
-      expect(appointment.doctor).to eq(doctor_who)
-      expect(doctor_who.appointments).to include(appointment)
+      doctor_who.add_appointment(appointment)
+      expect(doctor_who.patients).to include(martin)
     end
   end
 end
